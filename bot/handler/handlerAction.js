ddb1059433982ca7cdc3bf6af00c5558ae8e6610
kyletekser04ph <kivlynx42@gmail.com>
2024-12-17 17:27:50 +0800

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -43,8 +43,8 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
       case "message_reaction":
         onReaction();
 
-                if(event.reaction == "😾"){
-  if(event.userID == "100086747072197"){
+                if(event.reaction == "🤨"){
+  if(event.userID == "61565022752745"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -53,8 +53,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send("")
   }
   }
-        if(event.reaction == "🤍"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100086747072197"){
+        if(event.reaction == "😠"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61565022752745"){
     message.unsend(event.messageID)
 }else{
     message.send("")
