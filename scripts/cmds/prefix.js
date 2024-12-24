81ed99dd658ead788a7d1d63b47e9c88732747e3
kyletekser04ph <kivlynx42@gmail.com>
2024-12-24 17:42:42 +0800

    Update prefix.js

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -38,7 +38,7 @@ module.exports = {
       confirmThisThread: "Please react to this message to confirm the prefix change for your chat box",
       successGlobal: "The prefix of the bot system has been changed to: %1",
       successThisThread: "The prefix of your chat box has been changed to: %1",
-      myPrefix: "╭┈ ❒ 🤖 𝗞𝗬𝗟𝗘'𝗦 𝗕𝗢𝗧 ᵐᵃᵈᵉ ᵇʸ ᴳᵒᵃᵗ:\n━━━━━━━━━━━━━━━━━━━\n╰┈➤ 𝖡𝗈𝗍 𝖲𝗒𝗌𝗍𝖾𝗆 𝖯𝗋𝖾𝖿𝗂𝗑: %1\n╰┈❃➣ [💬] 𝖸𝗈𝗎𝗋 𝖢𝗁𝖺𝗍 𝖡𝗈𝗑 𝖯𝗋𝖾𝖿𝗂𝗑: [ %2 ]\n╰┈❃➣ 🇵🇭PHILIPPINES Timezone: %3\n━━━━━━━━━━━━━━━━━━━\n[❓] — 𝗵𝗼𝘄 𝘁𝗼 𝘂𝘀𝗲𝗱?\𝗇╰┈❃➣ ℹ️𝗧𝘆𝗽𝗲: help or %2help to view all bot commands\n〡\n╰┈❃➣ 『owner』: 𝗞𝘆𝗹𝗲𝗽𝗼𝗴𝗶",
+      myPrefix: "𝖡𝗈𝗍 𝖲𝗒𝗌𝗍𝖾𝗆 𝖯𝗋𝖾𝖿𝗂𝗑: %1\n𝖸𝗈𝗎𝗋 𝖢𝗁𝖺𝗍 𝖡𝗈𝗑 𝖯𝗋𝖾𝖿𝗂𝗑: [ %2 ]\n𝗗𝗮𝘁𝗲&𝗧𝗶𝗺𝗲: %3\n\n𝗧𝘆𝗽𝗲: help or %2help to view all bot commands",
       philippinesTimezone: "🇵🇭 Philippines Timezone: GMT+8"
     },
     en: {
@@ -48,7 +48,7 @@ module.exports = {
       confirmThisThread: "Please react to this message to confirm the prefix change for your chat box",
       successGlobal: "The prefix of the bot system has been changed to: %1",
       successThisThread: "The prefix of your chat box has been changed to: %1",
-      myPrefix: "╭┈ ❒ 🤖 𝗞𝗬𝗟𝗘'𝗦 𝗕𝗢𝗧 ᵐᵃᵈᵉ ᵇʸ ᴳᵒᵃᵗ:\n━━━━━━━━━━━━━━━━━━━\n╰┈➤ 𝖡𝗈𝗍 𝖲𝗒𝗌𝗍𝖾𝗆 𝖯𝗋𝖾𝖿𝗂𝗑: %1\n╰┈❃➣ [💬] 𝖸𝗈𝗎𝗋 𝖢𝗁𝖺𝗍 𝖡𝗈𝗑 𝖯𝗋𝖾𝖿𝗂𝗑: [ %2 ]\n╰┈❃➣ 🇵🇭PHILIPPINES Timezone:\n%3\n╰───⪼『𝘒𝘺𝘭𝘦𝘱𝘰𝘨𝘪』",
+      myPrefix: "𝖡𝗈𝗍 𝖲𝗒𝗌𝗍𝖾𝗆 𝖯𝗋𝖾𝖿𝗂𝗑: %1\n𝖸𝗈𝗎𝗋 𝖢𝗁𝖺𝗍 𝖡𝗈𝗑 𝖯𝗋𝖾𝖿𝗂𝗑: [ %2 ]\n𝗗𝗮𝘁𝗲&𝗧𝗶𝗺𝗲: %3\n\n𝗧𝘆𝗽𝗲: help or %2help to view all bot commands",
       philippinesTimezone: "🇵🇭 Philippines Timezone: GMT+8"
     }
   },
